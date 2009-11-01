21441d8e295d8a0a8611e32149248dae4ee4e04c
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2009-11-01 18:04:24 +0000

    AppServer erwartet zwei Parameter beim Starten. (appId = Application Id und ctrlPort = Port auf dem der Controller mit dem AppServer kommuniziert)
    git-svn-id: svn://localhost/twospot_svn/trunk@172 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- AppServer/src/org/prot/appserver/Main.java
+++ AppServer/src/org/prot/appserver/Main.java
@@ -3,6 +3,14 @@ package org.prot.appserver;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.GnuParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Option;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.xml.XmlConfiguration;
 import org.xml.sax.SAXException;
@@ -18,7 +26,7 @@ public class Main {
 			server.start();
 
 			new Monitor();
-			
+
 		} catch (SAXException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -28,10 +36,36 @@ public class Main {
 		}
 	}
 
+	private static void parseArguments(String[] args) {
+		Options options = new Options();
+
+		Option appId = OptionBuilder.withArgName("application id").hasArg().isRequired().create("appId");
+		options.addOption(appId);
+
+		Option controlPort = OptionBuilder.withArgName("control port").hasArg().isRequired().create(
+				"ctrlPort");
+		options.addOption(controlPort);
+
+		try {
+			CommandLineParser parser = new GnuParser();
+			CommandLine cmd = parser.parse(options, args);
+
+			System.out.println("AppId: " + cmd.getOptionValue("appId"));
+			System.out.println("ControlPort: " +cmd.getOptionValue("ctrlPort"));
+
+		} catch (ParseException e) {
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp("AppServer", options);
+		}
+	}
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		new Main();
+		parseArguments(args);
+		
+		// TODO: use spring ioc
+		// new Main();
 	}
 }
