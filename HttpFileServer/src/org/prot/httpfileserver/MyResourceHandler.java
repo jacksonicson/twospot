4aa60e539850c46cec766ca90d55002eda155431
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2009-11-03 22:47:13 +0000

    FileServer lädt WAR-Files unter Angabe der AppId
    git-svn-id: svn://localhost/twospot_svn/trunk@197 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- HttpFileServer/src/org/prot/httpfileserver/MyResourceHandler.java
+++ HttpFileServer/src/org/prot/httpfileserver/MyResourceHandler.java
@@ -1,6 +1,9 @@
 package org.prot.httpfileserver;
 
 import java.io.File;
+import java.net.MalformedURLException;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.util.log.Log;
@@ -8,6 +11,25 @@ import org.eclipse.jetty.util.resource.Resource;
 
 public class MyResourceHandler extends ResourceHandler
 {
+	
+	protected Resource getResource(HttpServletRequest request) throws MalformedURLException
+    {
+        String path_info=request.getPathInfo();
+        String[] components = path_info.split("/");
+        
+        System.out.println("length: " + components.length); 
+        for(String s : components)
+        	System.out.println("S: " + s); 
+        
+        if(components.length == 3 && components[1].equals("app")) {
+        	String appId = components[2]; 
+        	String file = "/" + appId + ".war";
+        	System.out.println("loading: " + file); 
+        	return getResource(file); 
+        } 
+        
+        return getResource("/failed.txt"); 
+    }
 
 	public void setResourceBase(String resourceBase)
 	{
