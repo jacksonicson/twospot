8c038849cee163dbff9840fa8bb09cf8975a6030
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2009-11-03 22:34:26 +0000

    Einfacher File-Server geht. Modifikation beim Laden der Base-Resource
    git-svn-id: svn://localhost/twospot_svn/trunk@196 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- HttpFileServer/src/org/prot/httpfileserver/MyResourceHandler.java
+++ HttpFileServer/src/org/prot/httpfileserver/MyResourceHandler.java
@@ -1,8 +1,28 @@
 package org.prot.httpfileserver;
 
+import java.io.File;
+
 import org.eclipse.jetty.server.handler.ResourceHandler;
+import org.eclipse.jetty.util.log.Log;
+import org.eclipse.jetty.util.resource.Resource;
 
 public class MyResourceHandler extends ResourceHandler
 {
 
+	public void setResourceBase(String resourceBase)
+	{
+		try
+		{
+			File file = new File(resourceBase);
+			if(file.exists())
+				setBaseResource(Resource.newResource(file.getAbsolutePath()));
+			else
+				throw new IllegalArgumentException(resourceBase); 
+		} catch (Exception e)
+		{
+			Log.warn(e.toString());
+			Log.debug(e);
+			throw new IllegalArgumentException(resourceBase);
+		}
+	}
 }
