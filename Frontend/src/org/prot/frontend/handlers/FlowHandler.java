eddc55d18adbec1b6d19041950bb7f279ecf81ec
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2009-11-05 20:27:10 +0000

    FrontEnd und Manager über RMI verbunden
    git-svn-id: svn://localhost/twospot_svn/trunk@229 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- Frontend/src/org/prot/frontend/handlers/FlowHandler.java
+++ Frontend/src/org/prot/frontend/handlers/FlowHandler.java
@@ -8,15 +8,24 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.handler.AbstractHandler;
+import org.prot.manager.service.frontend.FrontendService;
 
 public class FlowHandler extends AbstractHandler
 {
+	private FrontendService frontendService; 
 	
 	@Override
 	public void handle(String target, Request baseRequest, HttpServletRequest request,
 			HttpServletResponse response) throws IOException, ServletException
 	{
 
+		System.out.println("Flow Handler"); 
+		frontendService.chooseAppServer("helloworld"); 
+		
 	}
-	
+
+	public void setFrontendService(FrontendService frontendService)
+	{
+		this.frontendService = frontendService;
+	}
 }
