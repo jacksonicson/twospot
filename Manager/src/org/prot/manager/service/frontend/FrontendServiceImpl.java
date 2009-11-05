704a86ca1838f7e286a78345e32dedef089c677d
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2009-11-05 20:27:08 +0000

    FrontEnd und Manager über RMI verbunden
    git-svn-id: svn://localhost/twospot_svn/trunk@228 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- Manager/src/org/prot/manager/service/frontend/FrontendServiceImpl.java
+++ Manager/src/org/prot/manager/service/frontend/FrontendServiceImpl.java
@@ -1,12 +1,17 @@
 package org.prot.manager.service.frontend;
 
+import org.apache.log4j.Logger;
 import org.prot.manager.pojos.AppServerInfo;
 
 public class FrontendServiceImpl implements FrontendService
 {
+	private static final Logger logger = Logger.getLogger(FrontendServiceImpl.class); 
+	
 	@Override
 	public AppServerInfo chooseAppServer(String appId)
 	{
+		logger.debug("chooseAppServer"); 
+		
 		AppServerInfo info = new AppServerInfo(); 
 		info.setControllerAddress("127.0.0.1");
 		info.setControllerPort(8080); 
