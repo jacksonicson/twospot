4b30d92f2fb9c7c7dee116fba3cda5c12f29fae3
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2009-11-06 00:42:18 +0000

    Ältere Version vom Spring-Framework in den Libs. Universeller Proxy-Handler. Neuer Proxy-Handler im Frontend getestet.
    git-svn-id: svn://localhost/twospot_svn/trunk@236 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- Frontend/src/org/prot/frontend/handlers/FlowHandler.java
+++ Frontend/src/org/prot/frontend/handlers/FlowHandler.java
@@ -1,31 +1,65 @@
 package org.prot.frontend.handlers;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.jetty.http.HttpURI;
 import org.eclipse.jetty.server.Request;
-import org.eclipse.jetty.server.handler.AbstractHandler;
 import org.prot.manager.service.frontend.FrontendService;
+import org.prot.util.handler.HttpProxyHandler;
 
-public class FlowHandler extends AbstractHandler
+public class FlowHandler extends HttpProxyHandler
 {
 	private FrontendService frontendService; 
+	
+	public FlowHandler() {
+		setup(); 
+	}
 	
 	@Override
 	public void handle(String target, Request baseRequest, HttpServletRequest request,
 			HttpServletResponse response) throws IOException, ServletException
 	{
 
-		System.out.println("Flow Handler"); 
-		frontendService.chooseAppServer("helloworld"); 
+//		System.out.println("Flow Handler"); 
+//		frontendService.chooseAppServer("helloworld"); 
 		
+		try
+		{
+			forwardRequest(baseRequest, request, response);
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
 	}
 
 	public void setFrontendService(FrontendService frontendService)
 	{
 		this.frontendService = frontendService;
 	}
+
+	@Override
+	protected String getHost(Request request)
+	{
+		return "www.heise.de";
+	}
+
+	@Override
+	protected String getUri(Request request)
+	{
+		return request.getUri().toString();
+	}
+
+	@Override
+	protected String getUrl(Request request)
+	{
+		return "http://www.heise.de";
+	}
+
+
 }
