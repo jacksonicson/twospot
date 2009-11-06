629032b6570708a4d307c5cb804d8ba532eda7af
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2009-11-06 21:39:29 +0000

    Django wird über WSGI gestartet (findet die Templates nicht)
    git-svn-id: svn://localhost/twospot_svn/trunk@250 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- HelloWorldDjango/src/hellopython/test.py
+++ HelloWorldDjango/src/hellopython/test.py
@@ -1,1 +1,30 @@
 print "TODO: use the new wsgiAdapter to start django here!!!"
+
+import os
+from wsgiAdapter import run_wsgi_app
+
+
+
+
+
+
+# Must set this env var before importing any part of Django
+# 'project' is the name of the project created with django-admin.py
+os.environ['DJANGO_SETTINGS_MODULE'] = 'hellopython.settings'
+
+# Force Django to reload its settings.
+from django.conf import settings
+settings._target = None
+
+import django.core.handlers.wsgi
+import django.core.signals
+import django.db
+import django.dispatch.dispatcher
+
+app = django.core.handlers.wsgi.WSGIHandler()
+
+set(wsgio_in)
+wsgio = wsgio_in
+print wsgio.readline()
+
+run_wsgi_app(app)
