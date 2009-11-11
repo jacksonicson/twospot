70debd3e15bc92879d595a3a403e9a9185459985
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2009-11-11 18:58:50 +0000

    HttpClient-Instanzen werden über die Spring-Konfigurationsdatei als Beans angelegt. Aufbau vom Frontend etwas eleganter.
    git-svn-id: svn://localhost/twospot_svn/trunk@322 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- HelloWorldServlet/src/org/prot/helloworldservlet/HelloWorldServlet.java
+++ HelloWorldServlet/src/org/prot/helloworldservlet/HelloWorldServlet.java
@@ -10,7 +10,9 @@ import javax.servlet.http.HttpServletResponse;
 public class HelloWorldServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+		
 		resp.setContentType("text/plain");
 		resp.getWriter().println("Hello, world");
+		resp.getWriter().close(); 
 	}
 }
