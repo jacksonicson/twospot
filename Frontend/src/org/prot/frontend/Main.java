dc2e854d33582a175c2d6d084397311ae2a4443b
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2009-11-11 20:23:00 +0000

    Properties-File im Frontend
    git-svn-id: svn://localhost/twospot_svn/trunk@334 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- Frontend/src/org/prot/frontend/Main.java
+++ Frontend/src/org/prot/frontend/Main.java
@@ -3,6 +3,7 @@ package org.prot.frontend;
 import org.apache.log4j.Logger;
 import org.apache.log4j.xml.DOMConfigurator;
 import org.eclipse.jetty.server.Server;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 
@@ -15,9 +16,13 @@ public class Main
 		// Configure logger
 		DOMConfigurator.configure(Main.class.getResource("/etc/log4j.xml"));
 
-		// Start spring ioc container
+		// Start spring IOC container
 		XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("/etc/spring.xml", getClass()));
 
+		// Postprocess the factory
+		PropertyPlaceholderConfigurer conf = (PropertyPlaceholderConfigurer)factory.getBean("PropertyConfigurer");
+		conf.postProcessBeanFactory(factory);
+		
 		// Start the server
 		Server server = (Server) factory.getBean("Server");
 		try
