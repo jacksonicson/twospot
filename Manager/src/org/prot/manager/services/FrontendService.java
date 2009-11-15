4af3a8a32c4e218289c030018ebf35617248d33c
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2009-11-15 18:29:52 +0000

    Manager schreibt seine Verbindungsdaten in den ZooKeeper-Service. Frontend lädt die Daten aus dem ZooKeeper-Service. Bei Abstürzen lässt sich das Frontend vom ZooKeeper informieren, bis der ZNode "/manager" erstellt wird. RMI-Connection kann nicht mehr über die ProxyFactory von Spring aufgebaut werden, da sich die Verbindungsdaten jederzeit ändern können. Frontend ist weitgehend robust gegen RMI-Kommunikationsfehler, liefert entsprechende HTTP-Status-Meldungen.
    git-svn-id: svn://localhost/twospot_svn/trunk@390 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- Manager/src/org/prot/manager/services/FrontendService.java
+++ Manager/src/org/prot/manager/services/FrontendService.java
@@ -5,6 +5,11 @@ import org.prot.manager.exceptions.MissingControllerException;
 
 public interface FrontendService
 {
+	/**
+	 * @param appId
+	 * @return null if there was an error or the controller information
+	 * @throws MissingControllerException
+	 */
 	public ControllerInfo chooseAppServer(String appId) throws MissingControllerException;
 	
 	public void newAppOrVersion(String appId); 
---
+++ Manager/src/org/prot/manager/zookeeper/ManagementService.java
@@ -0,0 +1,75 @@
+package org.prot.manager.zookeeper;
+
+import org.apache.log4j.Logger;
+import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.Stat;
+import org.prot.util.zookeeper.ZNodes;
+import org.prot.util.zookeeper.ZooHelper;
+
+public class ManagementService
+{
+	private static final Logger logger = Logger.getLogger(ManagementService.class);
+
+	// ZooKeeper helper
+	private ZooHelper zooHelper;
+
+	// Hostname of the master
+	private String host;
+
+	// Port of the master
+	private int port;
+
+	public void init()
+	{
+		try
+		{
+			registerMaster();
+		} catch (KeeperException e)
+		{
+			e.printStackTrace();
+		} catch (InterruptedException e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	private void registerMaster() throws KeeperException, InterruptedException
+	{
+		logger.debug("Registering this master with the ZooKeeper");
+
+		ZooKeeper zk = zooHelper.getZooKeeper();
+
+		Stat statMaster = zk.exists(ZNodes.ZNODE_MASTER, false);
+		if (statMaster == null)
+		{
+			byte[] address = (host + ":" + port).getBytes();
+			zk.create(ZNodes.ZNODE_MASTER, address, zooHelper.getACL(), CreateMode.EPHEMERAL);
+			statMaster = zk.exists(ZNodes.ZNODE_MASTER, true);
+			
+			logger.info("Master registered in ZooKeeper");
+			return;
+		} else
+		{
+			logger.error("ZooKeeper already contains a ZNode: " + ZNodes.ZNODE_MASTER
+					+ ". Multimaster is not supported");
+			System.exit(1);
+		}
+	}
+
+	public void setZooHelper(ZooHelper zooHelper)
+	{
+		this.zooHelper = zooHelper;
+	}
+
+	public void setHost(String host)
+	{
+		this.host = host;
+	}
+
+	public void setPort(int port)
+	{
+		this.port = port;
+	}
+}
