4ac6d8cdc94f3d3308b2c7ed1654cd1dab9025ae
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2009-11-17 23:36:50 +0000

    Hilfsklassen um die AppId zu überprüfen und aus URL's zu extrahieren
    git-svn-id: svn://localhost/twospot_svn/trunk@447 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- Util/src/org/prot/util/AppIdExtractor.java
+++ Util/src/org/prot/util/AppIdExtractor.java
@@ -1,6 +1,86 @@
 package org.prot.util;
 
-public class AppIdExtractor
+import org.apache.log4j.Logger;
+
+public final class AppIdExtractor
 {
-// TODO
+	private static final Logger logger = Logger.getLogger(AppIdExtractor.class);
+
+	/**
+	 * Extracts the AppId from URL's of the type: scheme://appId.domain:port/xxx
+	 * 
+	 * @param the
+	 *            complete url
+	 * @return the extracted appId
+	 */
+	public static String fromDomain(String url)
+	{
+		int index = -1;
+
+		// Remove the scheme part
+		index = url.indexOf("://");
+		if (index != -1)
+			url = url.substring(index + 3);
+
+		// Find the first dot and remove everything after it
+		index = url.indexOf(".");
+		if (index != -1)
+			url = url.substring(0, index);
+
+		// Check if the result is a valid AppId
+		if (!ReservedAppIds.validateAppId(url))
+			return null;
+
+		logger.debug("Extracted AppId: " + url);
+
+		// Return the AppId
+		return url;
+	}
+
+	/**
+	 * Extracts the AppId from URL's of the type: scheme://doamin:port/appId/xxx
+	 * 
+	 * @param the
+	 *            complete url
+	 * @return the extracted appId
+	 */
+	public static String fromUrlPath(String url)
+	{
+		int index = -1;
+
+		// Remove the scheme part
+		index = url.indexOf("://");
+		if (index != -1)
+			url = url.substring(index + 3);
+		
+		return fromUri(url);
+	}
+	
+	public static String fromUri(String uri)
+	{
+		int index = -1; 
+	
+		// Remove the first slash
+		if(uri.startsWith("/"))
+			uri = uri.substring(1);
+		
+		// Remove the domain and port part
+		index = uri.indexOf("/");
+		if (index != -1)
+			uri = uri.substring(index + 1);
+
+		// Find the next slash (if it exists) and remove everything after that
+		index = uri.indexOf("/");
+		if (index != -1)
+			uri = uri.substring(0, index);
+
+		// Check if the result is a valid AppId
+		if (!ReservedAppIds.validateAppId(uri))
+			return null;
+
+		logger.debug("Extracted AppId: " + uri);
+
+		// Return the AppId
+		return uri;
+	}
 }
---
+++ Util/src/org/prot/util/ReservedAppIds.java
@@ -0,0 +1,52 @@
+package org.prot.util;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.apache.log4j.Logger;
+
+public final class ReservedAppIds
+{
+	private static final Logger logger = Logger.getLogger(ReservedAppIds.class);
+	
+	public final static String APP_PORTAL = "portal";
+
+	public final static String FRONTEND_DEPLOY = "deploy";
+
+	private final static Set<String> reservedAppIds = new HashSet<String>();
+
+	static
+	{
+		reservedAppIds.add(APP_PORTAL);
+		reservedAppIds.add(FRONTEND_DEPLOY);
+	}
+
+	public static boolean isReserved(String appName)
+	{
+		return reservedAppIds.contains(appName);
+	}
+	
+	public static boolean validateAppId(String appId)
+	{
+		logger.debug("Validating AppId: " + appId); 
+		
+		// Check if this appId is reserved
+		if(ReservedAppIds.isReserved(appId))
+			return false;
+
+		// Check the length 
+		if(appId.length() > 10)
+			return false; 
+		
+		// Check each character of the appId
+		char[] chars = appId.toCharArray(); 
+		for(char c : chars)
+		{
+			if(!Character.isLetterOrDigit(c))
+				return false; 
+		}
+		
+		// Everything seems ok
+		return true; 	
+	}
+}
