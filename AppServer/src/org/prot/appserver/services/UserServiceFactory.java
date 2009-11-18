e82986e14025fe98a812c92c403ad1ffff13baaf
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2009-11-18 20:27:32 +0000

    UserSericeFactory verwendet PrivilegedUserService-Interface
    git-svn-id: svn://localhost/twospot_svn/trunk@457 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- AppServer/src/org/prot/appserver/services/UserServiceFactory.java
+++ AppServer/src/org/prot/appserver/services/UserServiceFactory.java
@@ -1,23 +1,24 @@
 package org.prot.appserver.services;
 
-import org.prot.controller.services.UserService;
+import org.prot.controller.services.user.PrivilegedUserService;
+import org.prot.controller.services.user.UserService;
 import org.springframework.remoting.rmi.RmiProxyFactoryBean;
 
 public class UserServiceFactory
 {
-	private static UserService userService;
+	private static PrivilegedUserService userService;
 
 	public static UserService getUserService()
 	{
 		if (userService == null)
 		{
 			RmiProxyFactoryBean proxyFactory = new RmiProxyFactoryBean();
-			proxyFactory.setServiceInterface(UserService.class);
-			proxyFactory.setServiceUrl("rmi://localhost:2299/UserService");
+			proxyFactory.setServiceInterface(PrivilegedUserService.class);
+			proxyFactory.setServiceUrl("rmi://localhost:2299/appserver/UserService");
 			proxyFactory.afterPropertiesSet();
-			userService = (UserService) proxyFactory.getObject();
+			userService = (PrivilegedUserService) proxyFactory.getObject();
 		}
-		
-		return userService; 
+
+		return userService;
 	}
 }
