b297ffe6fc6c790e798684f00f36ea49cb2cf163
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2009-12-02 00:04:19 +0000

    Bean-Postprocessor
    git-svn-id: svn://localhost/twospot_svn/trunk@575 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- AppServer/src/org/prot/appserver/Main.java
+++ AppServer/src/org/prot/appserver/Main.java
@@ -1,10 +1,12 @@
 package org.prot.appserver;
 
 import java.security.Policy;
+import java.util.Properties;
 
 import org.apache.log4j.xml.DOMConfigurator;
 import org.prot.app.security.HardPolicy;
 import org.prot.appserver.config.Configuration;
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.core.io.ClassPathResource;
 
@@ -36,10 +38,17 @@ public class Main
 		// Create beans
 		XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("/etc/spring.xml", getClass()));
 
-		// TODO: Add a postprocess to fill the variables in the definition
+		// Create the configuration properties
+		Properties props = new Properties();
+		props.setProperty("appId", Configuration.getInstance().getAppId());
 
+		// Postprocess the factory
+		PropertyPlaceholderConfigurer configurer = new PropertyPlaceholderConfigurer();
+		configurer.setProperties(props);
+		configurer.postProcessBeanFactory(factory);
+
+		// Get the beans
 		factory.getBean("ManagementExporter");
-
 		factory.getBean("Lifecycle");
 	}
 
