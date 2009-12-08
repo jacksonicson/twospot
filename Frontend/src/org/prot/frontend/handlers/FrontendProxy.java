24ba65b3bb1273a6cbbffcd36655884cbcaf134f
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2009-12-08 14:33:15 +0000

    Frontend schickt Timeout-Fehler an den Browser wenn die Connection mit dem Controller wegen einem timeout geschlossen wird.
    git-svn-id: svn://localhost/twospot_svn/trunk@683 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- Frontend/src/org/prot/frontend/handlers/FrontendProxy.java
+++ Frontend/src/org/prot/frontend/handlers/FrontendProxy.java
@@ -11,6 +11,19 @@ import org.prot.util.handler.HttpProxyHelper;
 public class FrontendProxy extends HttpProxyHelper<HttpServletResponse>
 {
 
+	@Override
+	protected void expired(HttpServletResponse response)
+	{
+		try
+		{
+			response.sendError(HttpStatus.REQUEST_TIMEOUT_408,
+					"Connection with the Controller timed out");
+		} catch (IOException e1)
+		{
+			// Do nothing
+		}
+	}
+
 	protected boolean error(HttpServletResponse response, Throwable e)
 	{
 		// Frontend could not connect with the Controller
