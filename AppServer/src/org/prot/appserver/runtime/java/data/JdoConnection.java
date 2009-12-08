9fb6f83e6a79623fc12d73fa089077c3a02cac97
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2009-12-08 17:02:22 +0000

    Log-Statements beim Aufbau der HBase-Verbindung. Damit soll geprüft werden wie lang der Verbindungsaufbau dauert!
    git-svn-id: svn://localhost/twospot_svn/trunk@691 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- AppServer/src/org/prot/appserver/runtime/java/data/JdoConnection.java
+++ AppServer/src/org/prot/appserver/runtime/java/data/JdoConnection.java
@@ -4,19 +4,28 @@ import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
 
+import org.apache.log4j.Logger;
+
 public class JdoConnection
 {
+	private static Logger logger = Logger.getLogger(JdoConnection.class);
+
 	private static PersistenceManagerFactory pmf;
 	private static PersistenceManager pm;
-	
+
 	public void init()
 	{
+		logger.debug("Creating peristence manager factory");
 		pmf = JDOHelper.getPersistenceManagerFactory("etc/jdoDefault.properties");
-		pm = pmf.getPersistenceManager(); 
+
+		logger.debug("Creating persistence manager");
+		pm = pmf.getPersistenceManager();
+
+		logger.debug("Persistence done");
 	}
-	
+
 	public PersistenceManager getPersistenceManager()
 	{
-		return pm; 
+		return pm;
 	}
 }
