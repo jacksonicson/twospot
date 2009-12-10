b8a8a5fe19beda4970778607379be9ac6873aad2
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2009-12-10 14:42:07 +0000

    JDOConnection in den Utils
    git-svn-id: svn://localhost/twospot_svn/trunk@750 8d73be03-56cb-46cc-8948-98b08ae92ef2

---
+++ Util/src/org/prot/util/jdo/JdoConnection.java
@@ -0,0 +1,32 @@
+package org.prot.util.jdo;
+
+import java.util.Stack;
+
+import javax.jdo.JDOHelper;
+import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
+
+public class JdoConnection
+{
+	private static PersistenceManagerFactory pmf;
+
+	private Stack<PersistenceManager> pms = new Stack<PersistenceManager>();
+
+	public void init()
+	{
+		pmf = JDOHelper.getPersistenceManagerFactory("etc/jdoDefault.properties");
+	}
+
+	public synchronized PersistenceManager getPersistenceManager()
+	{
+		if (pms.isEmpty())
+			return pmf.getPersistenceManager();
+
+		return pms.pop();
+	}
+
+	public synchronized void releasePersistenceManager(PersistenceManager pm)
+	{
+		pms.push(pm);
+	}
+}
