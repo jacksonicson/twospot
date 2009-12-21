6bf269642aa9b1c5657279ab9e8465fc76465d55
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2009-12-21 12:20:48 +0000

    Implementierung vom InsertFieldManager gelöscht - darf auch nicht mehr verwendet werden.
    git-svn-id: svn://localhost/twospot_svn/trunk@883 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- TwoSpotJdo/src/org/prot/stor/hbase/HBaseInsertFieldManager.java
+++ TwoSpotJdo/src/org/prot/stor/hbase/HBaseInsertFieldManager.java
@@ -17,14 +17,9 @@ Contributors :
  ***********************************************************************/
 package org.prot.stor.hbase;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
-
 import org.apache.hadoop.hbase.client.Delete;
 import org.apache.hadoop.hbase.client.Put;
 import org.datanucleus.StateManager;
-import org.datanucleus.exceptions.NucleusException;
 import org.datanucleus.store.fieldmanager.AbstractFieldManager;
 
 public class HBaseInsertFieldManager extends AbstractFieldManager
@@ -42,206 +37,61 @@ public class HBaseInsertFieldManager extends AbstractFieldManager
 
 	public void storeBooleanField(int fieldNumber, boolean value)
 	{
-		String familyName = HBaseUtils.getFamilyName(sm.getClassMetaData(), fieldNumber);
-		String columnName = HBaseUtils.getQualifierName(sm.getClassMetaData(), fieldNumber);
-
-		try
-		{
-			ByteArrayOutputStream bos = new ByteArrayOutputStream();
-			ObjectOutputStream oos = new ObjectOutputStream(bos);
-			oos.writeBoolean(value);
-			oos.flush();
-			put.add(familyName.getBytes(), columnName.getBytes(), bos.toByteArray());
-			oos.close();
-			bos.close();
-		} catch (IOException e)
-		{
-			throw new NucleusException(e.getMessage(), e);
-		}
+		// Never use this method
+		assert (false);
 	}
 
 	public void storeByteField(int fieldNumber, byte value)
 	{
-		String familyName = HBaseUtils.getFamilyName(sm.getClassMetaData(), fieldNumber);
-		String columnName = HBaseUtils.getQualifierName(sm.getClassMetaData(), fieldNumber);
-		put.add(familyName.getBytes(), columnName.getBytes(), new byte[] { value });
+		// Never use this method
+		assert (false);
 	}
 
 	public void storeCharField(int fieldNumber, char value)
 	{
-		String familyName = HBaseUtils.getFamilyName(sm.getClassMetaData(), fieldNumber);
-		String columnName = HBaseUtils.getQualifierName(sm.getClassMetaData(), fieldNumber);
-		try
-		{
-			ByteArrayOutputStream bos = new ByteArrayOutputStream();
-			ObjectOutputStream oos = new ObjectOutputStream(bos);
-			oos.writeChar(value);
-			oos.flush();
-			put.add(familyName.getBytes(), columnName.getBytes(), bos.toByteArray());
-			oos.close();
-			bos.close();
-		} catch (IOException e)
-		{
-			throw new NucleusException(e.getMessage(), e);
-		}
+		// Never use this method
+		assert (false);
 	}
 
 	public void storeDoubleField(int fieldNumber, double value)
 	{
-		String familyName = HBaseUtils.getFamilyName(sm.getClassMetaData(), fieldNumber);
-		String columnName = HBaseUtils.getQualifierName(sm.getClassMetaData(), fieldNumber);
-		try
-		{
-			ByteArrayOutputStream bos = new ByteArrayOutputStream();
-			ObjectOutputStream oos = new ObjectOutputStream(bos);
-			oos.writeDouble(value);
-			oos.flush();
-			put.add(familyName.getBytes(), columnName.getBytes(), bos.toByteArray());
-			oos.close();
-			bos.close();
-		} catch (IOException e)
-		{
-			throw new NucleusException(e.getMessage(), e);
-		}
+		// Never use this method
+		assert (false);
 	}
 
 	public void storeFloatField(int fieldNumber, float value)
 	{
-		String familyName = HBaseUtils.getFamilyName(sm.getClassMetaData(), fieldNumber);
-		String columnName = HBaseUtils.getQualifierName(sm.getClassMetaData(), fieldNumber);
-		try
-		{
-			ByteArrayOutputStream bos = new ByteArrayOutputStream();
-			ObjectOutputStream oos = new ObjectOutputStream(bos);
-			oos.writeFloat(value);
-			oos.flush();
-			put.add(familyName.getBytes(), columnName.getBytes(), bos.toByteArray());
-			oos.close();
-			bos.close();
-		} catch (IOException e)
-		{
-			throw new NucleusException(e.getMessage(), e);
-		}
+		// Never use this method
+		assert (false);
 	}
 
 	public void storeIntField(int fieldNumber, int value)
 	{
-		String familyName = HBaseUtils.getFamilyName(sm.getClassMetaData(), fieldNumber);
-		String columnName = HBaseUtils.getQualifierName(sm.getClassMetaData(), fieldNumber);
-		try
-		{
-			ByteArrayOutputStream bos = new ByteArrayOutputStream();
-			ObjectOutputStream oos = new ObjectOutputStream(bos);
-			oos.writeInt(value);
-			oos.flush();
-			put.add(familyName.getBytes(), columnName.getBytes(), bos.toByteArray());
-			oos.close();
-			bos.close();
-		} catch (IOException e)
-		{
-			throw new NucleusException(e.getMessage(), e);
-		}
+		// Never use this method
+		assert (false);
 	}
 
 	public void storeLongField(int fieldNumber, long value)
 	{
-		String familyName = HBaseUtils.getFamilyName(sm.getClassMetaData(), fieldNumber);
-		String columnName = HBaseUtils.getQualifierName(sm.getClassMetaData(), fieldNumber);
-		try
-		{
-			ByteArrayOutputStream bos = new ByteArrayOutputStream();
-			ObjectOutputStream oos = new ObjectOutputStream(bos);
-			oos.writeLong(value);
-			oos.flush();
-
-			// Prevent oversized uploads
-			if (!HBaseUtils.checkFieldSize(bos.size()))
-				throw new IOException("Maximum field size exceeded");
-
-			put.add(familyName.getBytes(), columnName.getBytes(), bos.toByteArray());
-
-			oos.close();
-			bos.close();
-		} catch (IOException e)
-		{
-			throw new NucleusException(e.getMessage(), e);
-		}
+		// Never use this method
+		assert (false);
 	}
 
 	public void storeObjectField(int fieldNumber, Object value)
 	{
-		String familyName = HBaseUtils.getFamilyName(sm.getClassMetaData(), fieldNumber);
-		String columnName = HBaseUtils.getQualifierName(sm.getClassMetaData(), fieldNumber);
-		if (value == null)
-		{
-			delete.deleteColumn(familyName.getBytes(), columnName.getBytes());
-		} else
-		{
-			try
-			{
-				ByteArrayOutputStream bos = new ByteArrayOutputStream();
-				ObjectOutputStream oos = new ObjectOutputStream(bos);
-				oos.writeObject(value);
-
-				// Prevent oversized uploads
-				if (!HBaseUtils.checkFieldSize(bos.size()))
-					throw new IOException("Maximum field size exceeded");
-
-				put.add(familyName.getBytes(), columnName.getBytes(), bos.toByteArray());
-				oos.close();
-				bos.close();
-			} catch (IOException e)
-			{
-				throw new NucleusException(e.getMessage(), e);
-			}
-		}
+		// Never use this method
+		assert (false);
 	}
 
 	public void storeShortField(int fieldNumber, short value)
 	{
-		String familyName = HBaseUtils.getFamilyName(sm.getClassMetaData(), fieldNumber);
-		String columnName = HBaseUtils.getQualifierName(sm.getClassMetaData(), fieldNumber);
-		try
-		{
-			ByteArrayOutputStream bos = new ByteArrayOutputStream();
-			ObjectOutputStream oos = new ObjectOutputStream(bos);
-			oos.writeShort(value);
-			oos.flush();
-			put.add(familyName.getBytes(), columnName.getBytes(), bos.toByteArray());
-			oos.close();
-			bos.close();
-		} catch (IOException e)
-		{
-			throw new NucleusException(e.getMessage(), e);
-		}
+		// Never use this method
+		assert (false);
 	}
 
 	public void storeStringField(int fieldNumber, String value)
 	{
-		String familyName = HBaseUtils.getFamilyName(sm.getClassMetaData(), fieldNumber);
-		String columnName = HBaseUtils.getQualifierName(sm.getClassMetaData(), fieldNumber);
-		if (value == null)
-		{
-			delete.deleteColumn(familyName.getBytes(), columnName.getBytes());
-		} else
-		{
-			try
-			{
-				ByteArrayOutputStream bos = new ByteArrayOutputStream();
-				ObjectOutputStream oos = new ObjectOutputStream(bos);
-				oos.writeObject(value);
-
-				// Prevent oversized uploads
-				if (!HBaseUtils.checkFieldSize(bos.size()))
-					throw new IOException("Maximum field size exceeded");
-
-				put.add(familyName.getBytes(), columnName.getBytes(), bos.toByteArray());
-				oos.close();
-				bos.close();
-			} catch (IOException e)
-			{
-				throw new NucleusException(e.getMessage(), e);
-			}
-		}
+		// Never use this method
+		assert (false);
 	}
 }
