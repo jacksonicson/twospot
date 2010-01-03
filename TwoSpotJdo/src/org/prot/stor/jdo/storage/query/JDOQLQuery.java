c192632d497418adce097e163aa4614a71d2d941
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2010-01-03 18:56:47 +0000

    JavaQueryEvaluator wird über die candidates vom JDOQL angewendet
    git-svn-id: svn://localhost/twospot_svn/trunk@969 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- TwoSpotJdo/src/org/prot/stor/jdo/storage/query/JDOQLQuery.java
+++ TwoSpotJdo/src/org/prot/stor/jdo/storage/query/JDOQLQuery.java
@@ -19,6 +19,7 @@ package org.prot.stor.jdo.storage.query;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
@@ -30,6 +31,8 @@ import org.datanucleus.ObjectManager;
 import org.datanucleus.StateManager;
 import org.datanucleus.exceptions.NucleusException;
 import org.datanucleus.metadata.AbstractClassMetaData;
+import org.datanucleus.query.evaluator.JDOQLEvaluator;
+import org.datanucleus.query.evaluator.JavaQueryEvaluator;
 import org.datanucleus.store.FieldValues;
 import org.datanucleus.store.query.AbstractJDOQLQuery;
 import org.prot.jdo.storage.StorageHelper;
@@ -142,7 +145,7 @@ public class JDOQLQuery extends AbstractJDOQLQuery
 			{
 				final ClassLoaderResolver clr2 = clr;
 				final FetchFieldManager manager = new FetchFieldManager(in, om, clr);
-				Class cls = clr.classForName(manager.getClassName());
+				Class<?> cls = clr.classForName(manager.getClassName());
 				final AbstractClassMetaData acmd = om.getMetaDataManager().getMetaDataForClass(cls, clr2);
 				obj = om.findObjectUsingAID(cls, new FieldValues()
 				{
@@ -168,22 +171,26 @@ public class JDOQLQuery extends AbstractJDOQLQuery
 					}
 
 				}, true, true);
-
-				// obj = om.findObjectUsingAID(arg0, arg1, arg2, arg3)
-				//				
-				//				
-				// obj = manager.get();
 			} catch (IOException e)
 			{
-				e.printStackTrace();
+				logger.error("Could not load entity", e);
 				continue;
 			}
 
 			if (obj == null)
+			{
+				logger.warn("Entity is null");
 				continue;
+			}
 
 			candidates.add(obj);
 		}
-		return candidates;
+
+		// Filter the candidates
+		JavaQueryEvaluator evaluator = new JDOQLEvaluator(this, candidates, compilation, parameters, om
+				.getClassLoaderResolver());
+		Collection results = evaluator.execute(true, true, true, true, true);
+
+		return results;
 	}
 }
