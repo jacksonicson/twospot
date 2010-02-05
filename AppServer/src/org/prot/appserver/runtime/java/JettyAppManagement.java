37250879ab5df924f00e6322816d47cccaa5239d
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2010-02-05 21:35:03 +0000

    Management-Daten werden im AppServer nicht mehr verarbeitet
    git-svn-id: svn://localhost/twospot_svn/trunk@1207 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- AppServer/src/org/prot/appserver/runtime/java/JettyAppManagement.java
+++ AppServer/src/org/prot/appserver/runtime/java/JettyAppManagement.java
@@ -1,8 +1,5 @@
 package org.prot.appserver.runtime.java;
 
-import java.util.LinkedList;
-
-import org.apache.log4j.Logger;
 import org.eclipse.jetty.server.Connector;
 import org.prot.appserver.management.RuntimeManagement;
 import org.prot.util.SystemStats;
@@ -12,130 +9,53 @@ import ort.prot.util.server.CountingRequestLog;
 
 public class JettyAppManagement implements RuntimeManagement
 {
-	private static final Logger logger = Logger.getLogger(JettyAppManagement.class);
+	private long startTime = System.currentTimeMillis();
+
+	private long lastPoll = 0;
 
 	private SystemStats systemStats = new SystemStats();
-
-	private long startTime = System.currentTimeMillis();
 
 	private CountingRequestLog countingRequestLog;
 
 	private Connector connector;
-
-	private int overloadCounter = 0;
-
-	private class Data
-	{
-		long startTime = 0;
-		long stopTime = 0;
-		long maxRequests = 0;
-		long currentRequests = 0;
-		long requestCounter = 0;
-	}
-
-	private final int DATAS_LENGTH = 10;
-	private LinkedList<Data> datas = new LinkedList<Data>();
-	private Data current = null;
-
-	public JettyAppManagement()
-	{
-		newData();
-	}
-
-	private void newData()
-	{
-		Data data = new Data();
-		data.startTime = System.currentTimeMillis();
-
-		datas.add(data);
-		current = data;
-	}
-
-	private void rollStats()
-	{
-		long time = System.currentTimeMillis() - current.startTime;
-		if (time > 5000)
-		{
-			current.stopTime = System.currentTimeMillis();
-
-			if (datas.size() >= DATAS_LENGTH)
-				datas.remove(0);
-
-			newData();
-
-			countingRequestLog.reset();
-			if (connector.isStarted())
-				connector.statsReset();
-		}
-	}
-
-	private void updateData()
-	{
-		current.stopTime = System.currentTimeMillis();
-
-		current.currentRequests = connector.getRequests();
-		if (current.maxRequests < current.currentRequests)
-			current.maxRequests = current.currentRequests;
-
-		current.requestCounter = countingRequestLog.getCounter();
-	}
-
-	private boolean isOverloaded()
-	{
-		boolean lowResources = connector.isLowResources();
-		if (lowResources)
-		{
-			overloadCounter = 5;
-			return lowResources;
-		} else
-		{
-			lowResources = overloadCounter > 0;
-			overloadCounter--;
-			return lowResources;
-		}
-	}
-
-	private float averageRps()
-	{
-		long counter = 0;
-		long minTime = Long.MAX_VALUE;
-		long maxTime = 0;
-
-		for (Data data : datas)
-		{
-			counter += data.requestCounter;
-
-			if (minTime > data.startTime)
-				minTime = data.startTime;
-
-			if (maxTime < data.stopTime)
-				maxTime = data.stopTime;
-		}
-
-		if (datas.size() > 0)
-			return (float) counter / ((float) (maxTime - minTime) / 1000f);
-
-		return 0f;
-	}
 
 	private long getRuntime()
 	{
 		return System.currentTimeMillis() - startTime;
 	}
 
+	private float getRps()
+	{
+		long time = System.currentTimeMillis() - lastPoll;
+		double rps = (double) countingRequestLog.getCounter() / (double) (time / 1000);
+		return (float) rps;
+	}
+
+	private float getDelay()
+	{
+		double delay = (double) countingRequestLog.getSummedRequestTime()
+				/ (double) countingRequestLog.getCounter();
+		return (float) delay;
+	}
+
+	private boolean isLowResources()
+	{
+		return connector.isLowResources();
+	}
+
 	@Override
 	public void fill(AppServer.Builder appServer)
 	{
-		updateData();
-
 		appServer.setRuntime(getRuntime());
 		appServer.setProcCpu(systemStats.getProcessLoadSinceLastCall());
 		appServer.setCpuTotal(systemStats.getCpuTotal());
 		appServer.setCpuProcTotal(systemStats.getProcTotal());
-		appServer.setRps(averageRps());
-		appServer.setOverloaded(isOverloaded());
+		appServer.setRps(getRps());
+		appServer.setAverageDelay(getDelay());
+		appServer.setOverloaded(isLowResources());
 
-		rollStats();
+		lastPoll = System.currentTimeMillis();
+		countingRequestLog.reset();
 	}
 
 	public void setCountingRequestLog(CountingRequestLog countingRequestLog)
