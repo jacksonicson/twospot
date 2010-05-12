3083282a24243b508c53d5956a825bc2035467b0
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2010-05-12 21:41:43 +0000

    Parameter von Winstone leicht modifiziert. Startup wird gegenüber Jetty aber nicht wesentlich beschleunigt.
    git-svn-id: svn://localhost/twospot_svn/trunk@1927 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- Controller/src/org/prot/controller/app/PortPool.java
+++ Controller/src/org/prot/controller/app/PortPool.java
@@ -1,13 +1,13 @@
 package org.prot.controller.app;
 
 import java.net.ServerSocket;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.Queue;
 
 import org.apache.log4j.Logger;
 
-public class PortPool
-{
+public class PortPool {
 	private static final Logger logger = Logger.getLogger(PortPool.class);
 
 	private final int startPort = 9090;
@@ -16,41 +16,42 @@ public class PortPool
 
 	private Queue<Integer> freePorts = new LinkedList<Integer>();
 
-	public void releasePort(int port)
-	{
-		freePorts.add(port);
+	private Queue<Integer> usedPorts = new LinkedList<Integer>();
+
+	public void releasePort(int port) {
+		usedPorts.add(port);
+		for (Iterator<Integer> it = usedPorts.iterator(); it.hasNext();) {
+			int testPort = it.next();
+			if (!portUsed(testPort)) {
+				it.remove();
+				freePorts.add(testPort);
+				
+				logger.info("Port is free: " + testPort); 
+			}
+		}
 	}
 
-	public synchronized int getPort()
-	{
+	private boolean portUsed(int port) {
+		try {
+			ServerSocket socket = new ServerSocket(port);
+			socket.close();
+			return true;
+		} catch (Exception e) {
+		}
+
+		return false;
+	}
+
+	public synchronized int getPort() {
 		// Check if there are any free ports
 		if (freePorts.isEmpty())
 			return this.currentPort++;
 
 		// Find a free port
-		synchronized (freePorts)
-		{
-			Integer foundPort = null;
-			for (Integer test : freePorts)
-			{
-				try
-				{
-					ServerSocket socket = new ServerSocket(test);
-					socket.close();
-					foundPort = test;
-					break;
-				} catch (Exception e)
-				{
-					logger.warn("AppRegsitry could not reuse port: " + test);
-					continue;
-				}
-			}
-
+		synchronized (freePorts) {
+			Integer foundPort = freePorts.poll();
 			if (foundPort != null)
-			{
-				freePorts.remove(foundPort);
 				return foundPort;
-			}
 		}
 
 		return this.currentPort++;
