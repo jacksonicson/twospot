55214e2e464493c0f7672d6130d5047b3de8318d
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2010-06-05 13:26:01 +0000

    Anbindung vom Java und V8-Server funktioniert
    git-svn-id: svn://localhost/twospot_svn/trunk@2034 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- Controller/src/org/prot/controller/app/lifecycle/appconfig/AppConfigurer.java
+++ Controller/src/org/prot/controller/app/lifecycle/appconfig/AppConfigurer.java
@@ -35,14 +35,15 @@ public class AppConfigurer {
 	private Map<?, ?> loadFile(String appDirectory) throws InvalidYamlFileException, IOException {
 		File yamlFile = new File(appDirectory + "/" + CONFIG_FILE);
 
+		Yaml yaml = new Yaml();
+
 		InputStream in = null;
-		Object yamlObj = null; 
+		Object yamlObj = null;
 		try {
 			in = new FileInputStream(yamlFile);
-			Yaml yaml = new Yaml();
 			yamlObj = yaml.load(in);
 		} catch (IOException e) {
-			throw e; 
+			throw e;
 		} finally {
 			if (in != null)
 				in.close();
