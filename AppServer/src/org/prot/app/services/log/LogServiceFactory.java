441b50f8ed5699e50f0379404a1c74c2bebdb8ee
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2010-06-07 19:05:02 +0000

    LogService auf ProtoBuf umgeschrieben
    git-svn-id: svn://localhost/twospot_svn/trunk@2049 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- AppServer/src/org/prot/app/services/log/LogServiceFactory.java
+++ AppServer/src/org/prot/app/services/log/LogServiceFactory.java
@@ -1,67 +1,59 @@
 package org.prot.app.services.log;
 
-import java.security.AccessController;
-import java.security.PrivilegedAction;
-
 import org.apache.log4j.Logger;
 import org.prot.appserver.config.Configuration;
-import org.springframework.remoting.rmi.RmiProxyFactoryBean;
 
-public class LogServiceFactory
-{
+public class LogServiceFactory {
 	private static final Logger logger = Logger.getLogger(LogServiceFactory.class);
 
 	private static final String CONTROLLER_ADDRESS = "localhost";
 
 	private static LogService logService;
 
-	private static final int getRmiPort()
-	{
+	private static final int getRmiPort() {
 		return Configuration.getInstance().getRmiRegistryPort();
 	}
 
-	private static LogService createLogService()
-	{
-		return createMockLogService();
-		
-//		LogService logService = AccessController.doPrivileged(new PrivilegedAction<LogService>()
-//		{
-//			public LogService run()
-//			{
-//				RmiProxyFactoryBean proxyFactory = new RmiProxyFactoryBean();
-//				proxyFactory.setServiceInterface(org.prot.controller.services.log.LogService.class);
-//				proxyFactory.setServiceUrl("rmi://" + CONTROLLER_ADDRESS + ":" + getRmiPort()
-//						+ "/appserver/LogService");
-//				proxyFactory.afterPropertiesSet();
-//
-//				Object object = proxyFactory.getObject();
-//				if (object == null)
-//				{
-//					logger.error("Could not connect with the DbServices");
-//					throw new NullPointerException();
-//				}
-//
-//				LogService service = new LogServiceImpl((org.prot.controller.services.log.LogService) object);
-//
-//				// Object o = userService;
-//				return service;
-//			}
-//		});
-//
-//		return logService;
+	private static LogService createLogService() {
+		return new LogServiceImpl();
+
+		// LogService logService = AccessController.doPrivileged(new
+		// PrivilegedAction<LogService>()
+		// {
+		// public LogService run()
+		// {
+		// RmiProxyFactoryBean proxyFactory = new RmiProxyFactoryBean();
+		// proxyFactory.setServiceInterface(org.prot.controller.services.log.LogService.class);
+		// proxyFactory.setServiceUrl("rmi://" + CONTROLLER_ADDRESS + ":" +
+		// getRmiPort()
+		// + "/appserver/LogService");
+		// proxyFactory.afterPropertiesSet();
+		//
+		// Object object = proxyFactory.getObject();
+		// if (object == null)
+		// {
+		// logger.error("Could not connect with the DbServices");
+		// throw new NullPointerException();
+		// }
+		//
+		// LogService service = new
+		// LogServiceImpl((org.prot.controller.services.log.LogService) object);
+		//
+		// // Object o = userService;
+		// return service;
+		// }
+		// });
+		//
+		// return logService;
 	}
 
-	private static LogService createMockLogService()
-	{
+	private static LogService createMockLogService() {
 		return new LogServiceMock();
 	}
 
-	public static LogService getLogService()
-	{
-		if (logService == null)
-		{
-			switch (Configuration.getInstance().getServerMode())
-			{
+	public static LogService getLogService() {
+		if (logService == null) {
+			switch (Configuration.getInstance().getServerMode()) {
 			case DEVELOPMENT:
 				logService = createMockLogService();
 				break;
--- AppServer/src/org/prot/app/services/log/LogServiceImpl.java
+++ AppServer/src/org/prot/app/services/log/LogServiceImpl.java
@@ -1,66 +1,111 @@
 package org.prot.app.services.log;
 
+import java.security.AccessController;
+import java.security.PrivilegedAction;
 import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 import org.prot.appserver.config.Configuration;
+import org.prot.controller.services.gen.Services.ListMessages;
+import org.prot.controller.services.gen.Services.MessageList;
+import org.prot.controller.services.gen.Services.Void;
 
-public final class LogServiceImpl implements LogService
-{
+import com.google.protobuf.RpcCallback;
+import com.googlecode.protobuf.socketrpc.SocketRpcChannel;
+import com.googlecode.protobuf.socketrpc.SocketRpcController;
+
+public final class LogServiceImpl implements LogService {
 	private static final Logger logger = Logger.getLogger(LogServiceImpl.class);
 
-	private final org.prot.controller.services.log.LogService logService;
+	private void log(final String message, final int severity) {
+		final String token = Configuration.getInstance().getAuthenticationToken();
+		final String appId = Configuration.getInstance().getAppId();
 
-	LogServiceImpl(org.prot.controller.services.log.LogService logService)
-	{
-		this.logService = logService;
+		AccessController.doPrivileged(new PrivilegedAction<Void>() {
+			private String retUrl;
+
+			@Override
+			public Void run() {
+				SocketRpcChannel socketRpcChannel = new SocketRpcChannel("localhost", 6548);
+				SocketRpcController rpcController = socketRpcChannel.newRpcController();
+
+				org.prot.controller.services.gen.Services.LogService stub = org.prot.controller.services.gen.Services.LogService
+						.newStub(socketRpcChannel);
+
+				org.prot.controller.services.gen.Services.LogMessage.Builder build = org.prot.controller.services.gen.Services.LogMessage
+						.newBuilder();
+				build.setAppid(appId);
+				build.setSeverity(severity);
+				build.setMessage(message);
+				build.setToken(token);
+
+				stub.log(rpcController, build.build(), new RpcCallback<Void>() {
+					@Override
+					public void run(Void arg0) {
+					}
+				});
+
+				return null;
+			}
+		});
 	}
 
-	private void log(String message, int severity)
-	{
-		String token = Configuration.getInstance().getAuthenticationToken();
-		String appId = Configuration.getInstance().getAppId();
-		logService.log(token, appId, message, severity);
+	public List<LogMessage> getMessages(final String appId, final int severity) {
+
+		final String token = Configuration.getInstance().getAuthenticationToken();
+
+		ArrayList<LogMessage> msgs = AccessController
+				.doPrivileged(new PrivilegedAction<ArrayList<LogMessage>>() {
+					private ArrayList<LogMessage> result;
+
+					@Override
+					public ArrayList<LogMessage> run() {
+						SocketRpcChannel socketRpcChannel = new SocketRpcChannel("localhost", 6548);
+						SocketRpcController rpcController = socketRpcChannel.newRpcController();
+
+						org.prot.controller.services.gen.Services.LogService stub = org.prot.controller.services.gen.Services.LogService
+								.newStub(socketRpcChannel);
+
+						ListMessages.Builder build = ListMessages.newBuilder();
+						build.setAppId(appId);
+						build.setSeverity(severity);
+						build.setToken(token);
+
+						stub.listMessages(rpcController, build.build(), new RpcCallback<MessageList>() {
+							@Override
+							public void run(MessageList list) {
+								result = new ArrayList<LogMessage>();
+								for (org.prot.controller.services.gen.Services.LogMessage msg : list
+										.getMessagesList()) {
+									LogMessage newMessage = new LogMessage();
+									newMessage.setMessage(msg.getMessage());
+									newMessage.setSeverity(msg.getSeverity());
+									result.add(newMessage);
+								}
+							}
+						});
+
+						return result;
+					}
+				});
+
+		return msgs;
 	}
 
-	public List<LogMessage> getMessages(String appId, int severity)
-	{
-		// TODO: This implementation is not very nice. The LogMessage class
-		// should be moved to the Utils where its available to the appserver,
-		// controller and apps
-		String token = Configuration.getInstance().getAuthenticationToken();
-		List<org.prot.controller.services.log.LogMessage> result = logService.getMessages(token, appId,
-				severity);
-		List<LogMessage> messages = new ArrayList<LogMessage>();
-		for (org.prot.controller.services.log.LogMessage copy : result)
-		{
-			LogMessage newMessage = new LogMessage();
-			newMessage.setMessage(copy.getMessage());
-			newMessage.setSeverity(copy.getSeverity());
-			messages.add(newMessage);
-		}
-
-		return messages;
-	}
-
-	public void debug(String message)
-	{
+	public void debug(String message) {
 		log(message, 0);
 	}
 
-	public void info(String message)
-	{
+	public void info(String message) {
 		log(message, 1);
 	}
 
-	public void warn(String message)
-	{
+	public void warn(String message) {
 		log(message, 2);
 	}
 
-	public void error(String message)
-	{
+	public void error(String message) {
 		log(message, 3);
 	}
 }
