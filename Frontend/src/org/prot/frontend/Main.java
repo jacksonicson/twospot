9fb8274bb103e93139bcefffb73f17973b6a3e24
wolke <wolke@8d73be03-56cb-46cc-8948-98b08ae92ef2>
2010-06-15 19:12:43 +0000

    EPL1.0 Header
    git-svn-id: svn://localhost/twospot_svn/trunk@2086 8d73be03-56cb-46cc-8948-98b08ae92ef2

--- Frontend/src/org/prot/frontend/Main.java
+++ Frontend/src/org/prot/frontend/Main.java
@@ -1,3 +1,13 @@
+/*******************************************************************************
+ * Copyright (c) 2010 Andreas Wolke.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Andreas Wolke - initial API and implementation and initial documentation
+ *******************************************************************************/
 package org.prot.frontend;
 
 import org.apache.log4j.Logger;
